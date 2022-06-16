8b8e77267fa28f397e0d264674b9fe96a589f0fa
AshenNeo <AshenNeo@gmail.com>
2022-06-17 00:08:42 +0900

    ・ビルドスクリプトに base-href オプションを記述 ・ビルドスクリプト確認のため、プロダクトを一つ追加

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,12 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 4,
+    name: 'Galaxy',
+    price: 350,
+    description: ''
   }
 ];
 
